a19a14af6d798b1055ccd808ba204e1a0792f92f
马金凤 <1021008546@qq.com>
2016-07-14 18:01:58 +0800

    下载图片并保存

--- GrabPicture.py
+++ GrabPicture.py
@@ -2,12 +2,17 @@
 import urllib2, os, sys
 from bs4 import BeautifulSoup
 
+
 class GrabPicture:
     def __init__(self):
         self.web_url = 'http://tieba.baidu.com/p/2166231880'
         self.picture_url = []
+        self.new_folder ='E:\GrabPicture'
 
-
+    def get_picture(self):
+        self.new_folder = self.creat_folder()
+        self.get_picture_url()
+        self.save_picture()
 
     def get_picture_url(self):
         res = urllib2.urlopen(self.web_url,timeout=1500)
@@ -17,11 +22,20 @@ class GrabPicture:
 
         print len(self.picture_url)
         print self.picture_url
+
     def creat_folder(self):
-        pass
+        if not os.path.isdir(self.new_folder):
+            os.mkdir(self.new_folder)
+        return self.new_folder
 
     def save_picture(self):
-        pass
-
+        i = 1
+        print self.new_folder
+        for picture in self.picture_url:
+            picture_str = urllib2.urlopen(picture).read()
+            with open(os.path.join(self.new_folder, 'pic%s' % i + '.jpg'), 'wb') as  picture_file:
+                picture_file.write(picture_str)
+            i += 1
+            
 p = GrabPicture()
-p.get_picture_url()
+p.get_picture()
